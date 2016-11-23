08970a92535fe3860c34acaff0812aa6d0b5f54d
Daniel <cosner.daniel@gmail.com>
2016-11-23 18:14:59 +0100

    TP projekt

---
+++ Info/Praxis/orderd/002 ue1JS/scripts/trump.js
@@ -0,0 +1,7 @@
+function changeTrump(x) {
+	var mtop=Math.random()*800;
+	var mleft=Math.random()*1600;
+	x.style.marginTop = mtop.toString()+"px";
+	x.style.marginLeft = mleft.toString()+"px";
+
+};
